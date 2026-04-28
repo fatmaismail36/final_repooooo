65853db5b43318e3c4aea0a6c4d0097924f4a949
nany246755 <nany246755@bue.edu.eg>
2026-04-28 22:13:26 +0300

    newmerge

--- SupplyChainSystem/src/supplychaintrackingsystem/ShipmentSubject.java
+++
@@ -1,9 +0,0 @@
-package supplychaintrackingsystem;
-
-public interface ShipmentSubject {
-    void registerObserver(ShipmentObserver observer);
-
-    void removeObserver(ShipmentObserver observer);
-
-    void notifyObservers(String message);
-}
--- SupplyChainSystem/src/supplychaintrackingsystem/SupplyChainTrackingSystem.java
+++
@@ -1,8 +0,0 @@
-package supplychaintrackingsystem;
-
-public class SupplyChainTrackingSystem {
-
-    public static void main(String[] args) {
-        java.awt.EventQueue.invokeLater(() -> new LoginGuii().setVisible(true));
-    }
-}
